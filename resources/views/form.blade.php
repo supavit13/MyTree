90b9e9c6124f349b66ba78735df2154183e4a4ac
witcha <dewdonvicha@gmail.com>
2017-11-25 20:46:13 +0700

    make it better

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1153,7 +1153,7 @@ function getData($tree,$type='',$name='',$var=NULL){
                 <thead>
                   <tr>
                     <th rowspan="2" class="rotate" style="width: 2em;">ลำดับที่</th>
-                    <th rowspan="2" class="rotate" style="width: 2em;">ส่วนของต้นไม้ที่เป็นอันตราย</th>
+                    <th rowspan="2" class="rotate" style="width: 3em !important;">ส่วนของต้นไม้ที่เป็นอันตราย</th>
                     <th rowspan="2" class="rotate">รายละเอียดของความเสี่ยง</th>
                     <th colspan="3" style="text-align: center;">โอกาสในการเกิด</th>
                     <th rowspan="2" class="rotate">ระดับความรุนแรงของผลกระทบ</th>
