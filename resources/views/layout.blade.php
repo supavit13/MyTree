1c872573b095d20dff41cc281be6610be5edb6bc
catter <terter1949@hotmail.com>
2017-11-27 04:13:38 +0700

    make it better

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -616,8 +616,9 @@ $(document).ready(function(){
 		    }
 		}
 	</script>
-	<?php $sizeofList =  sizeof($tree['listDamage'])-1 ;?>
+	
 	<script type="text/javascript">
+		<?php $sizeofList =  sizeof($tree['listDamage'])-1 ;?>
             var i = "<?php echo $sizeofList; ?>" ;
             $("#add_table").click(function(event){
               event.preventDefault();
