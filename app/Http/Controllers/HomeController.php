153858a01562cd64dece35e5f28d12301d27b542
witcha <dewdonvicha@gmail.com>
2017-12-12 04:24:21 +0700

    make it better

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -167,7 +167,7 @@ class HomeController extends Controller
             foreach ($request->fulltree as $value) {
                 foreach ($tree_id->Tree_imgFull as $key) {
                     if($value == $key){
-                        dd($value);
+                        dd(Storage::delete('images/uploads/'.$value));
                         Storage::delete('images/uploads/'.$value);
                     }
        
