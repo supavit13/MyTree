a1c553147b60c40822d6fa3bf2572e5d18042acd
supavit13 <supavit.n@ku.th>
2017-12-13 17:56:36 +0700

    mytree

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -347,12 +347,15 @@
       <script src="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.js"></script>
 
 
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.1.0/dist/leaflet.css"
+      <!-- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.1.0/dist/leaflet.css"
    integrity="sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw=="
    crossorigin=""/>
    <script src="https://unpkg.com/leaflet@1.1.0/dist/leaflet.js"
    integrity="sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA=="
-   crossorigin=""></script>
+   crossorigin=""></script> -->
+
+   <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.3.1/leaflet.css" />
+    <script src="http://cdn.leafletjs.com/leaflet-0.3.1/leaflet.js"></script>
 
 	<script
 	  src="https://code.jquery.com/jquery-3.1.1.min.js"
